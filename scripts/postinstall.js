3a9126345446fdda52962edefc2c02fa7e65d36f
codecheri <54028059+codecheri@users.noreply.github.com>
2020-04-27 16:44:11 +0200

    Remove iOS PostInstall Script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const ios_helpers = require('./ios.link_helpers');
+// const ios_helpers = require('./ios.link_helpers');
 
 // Install Pods
-console.log("react-native-launch-navigator: pod install");
-ios_helpers.podInstall();
+// console.log("react-native-launch-navigator: pod install");
+// ios_helpers.podInstall();
